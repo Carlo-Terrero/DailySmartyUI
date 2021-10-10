2788ebbfcfbfbcb473ea9775af577488e9520e63
Carlo-Terrero <carlosjose2111@gmail.com>
2021-10-10 11:45:48 +0100

    implementamos primeras funcionalidades de la barra de busqueda

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-export default class SearchBar extends Component {
+ class SearchBar extends Component {
+
+    handelFormSubmit = function({query}) {
+        //navegado a una nueva ruta
+        console.log('probando del boton enviar de la query', query);
+    }
+
+    renderInput(field){
+        return <input type="text" placeholder="Search DailySmarty" {... field.input} />
+    }
+
     render() {
+
+        const { handleSubmit } = this.props;
+
         return (
-           <form>
-               <input placeholder="Search Dailysma" />
-                
+           <form className="search-bar" onSubmit={handleSubmit(this.handelFormSubmit.bind(this))}>
+               <Field name="query" component={this.renderInput} /> 
            </form>
         );
     }
 }
+
+SearchBar = reduxForm({
+    form: 'searchBar'
+})(SearchBar);
+
+export default SearchBar;
